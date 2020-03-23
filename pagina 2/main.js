2ecf80bb36ea0fdc613794dc89fa6879da206280
Darkang <warioxz198@gmail.com>
2020-03-23 14:06:17 -0500

    hice una accion con el menu

--- pagina 2/main.js
+++ pagina 2/main.js
@@ -1,7 +1,16 @@
+var contador = 0;
+
 $(function mostrar_menu() {
 	$('#contenedor_menu').on('click', function() {
 		$(this)
 			.next()
 			.slideToggle();
+		contador++;
+		if (contador === 9) {
+			document
+				.querySelectorAll('.escondido')
+				.forEach((a) => (a.style.display = 'initial'));
+			alert('desbloqueaste algo interesante en el menu mira que es!!!');
+		}
 	});
 });
