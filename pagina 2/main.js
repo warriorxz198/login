bc3d14657385cdbb62c1b3f2289b581f3a627cab
Darkang <warioxz198@gmail.com>
2020-03-30 16:41:42 -0500

    hice que el menu pudiera cambiar a menu2 aun faltan animacionesy cambiar el objetivo de menu2

--- pagina 2/main.js
+++ pagina 2/main.js
@@ -1,9 +1,30 @@
 var contador = 0;
+var menu = document.getElementById('menu');
+var menu2 = document.getElementById('menu2');
+var boton1 = document.getElementById('boton_menu');
+var boton2 = document.getElementById('boton_left');
 
-$(function mostrar_menu() {
-	$('#contenedor_menu').on('click', function() {
+$(document).ready(function mostrar_menu() {
+	$('#boton_menu').on('click', function() {
 		$(this)
+			.parent()
 			.next()
+			.slideToggle();
+		contador++;
+
+		if (contador === 9) {
+			document
+				.querySelectorAll('.escondido')
+				.forEach((a) => (a.style.display = 'initial'));
+			alert('desbloqueaste algo interesante en el menu mira que es!!!');
+		}
+	});
+});
+
+$(function despeglar() {
+	$('.links').on('click', function() {
+		$(this)
+			.parent()
 			.slideToggle();
 		contador++;
 		if (contador === 9) {
@@ -15,18 +36,19 @@ $(function mostrar_menu() {
 	});
 });
 
-$(function despeglar() {
-	$('#menu').on('click', function() {
-		$(this).slideToggle();
-		contador++;
-		if (contador === 9) {
-			document
-				.querySelectorAll('.escondido')
-				.forEach((a) => (a.style.display = 'initial'));
-			alert('desbloqueaste algo interesante en el menu mira que es!!!');
-		}
-	});
-});
+function menu_2() {
+	if (menu2.style.display == 'block') {
+		menu.style.display = 'block';
+		menu2.style.display = 'none';
+		boton1.style.display = 'block';
+		boton2.style.display = 'none';
+	} else {
+		menu.style.display = 'none';
+		menu2.style.display = 'block';
+		boton1.style.display = 'none';
+		boton2.style.display = 'block';
+	}
+}
 
 var gatito = 0;
 
