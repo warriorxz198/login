7d0b35bb08848740aaeead088af2f2c23c722b95
Darkang <warioxz198@gmail.com>
2020-03-31 11:37:14 -0500

    hice el menu responsive ya es funcional en tablets y pc pero faltan mejoras y agragre nuevas funciones al menu2 ya hace algo cuando se hace click en el gato

--- pagina 2/main.js
+++ pagina 2/main.js
@@ -1,4 +1,5 @@
 var contador = 0;
+var contador2 = 0;
 var menu = document.getElementById('menu');
 var menu2 = document.getElementById('menu2');
 var boton1 = document.getElementById('boton_menu');
@@ -23,11 +24,25 @@ $(document).ready(function mostrar_menu() {
 
 $(function despeglar() {
 	$('.links').on('click', function() {
-		$(this)
-			.parent()
-			.slideToggle();
-		contador++;
-		if (contador === 9) {
+		if (screen.width <= 599) {
+			$(this)
+				.parent()
+				.slideToggle();
+			contador++;
+
+			if (contador === 9) {
+				document
+					.querySelectorAll('.escondido')
+					.forEach((a) => (a.style.display = 'initial'));
+				alert('desbloqueaste algo interesante en el menu mira que es!!!');
+			}
+		}
+
+		// esto es para que el menu2 funcione en dispotivos con un ancho mayor a 600px como una tablet //
+		else {
+			contador2++;
+		}
+		if (contador2 === 5) {
 			document
 				.querySelectorAll('.escondido')
 				.forEach((a) => (a.style.display = 'initial'));
@@ -37,24 +52,45 @@ $(function despeglar() {
 });
 
 function menu_2() {
-	if (menu2.style.display == 'block') {
-		menu.style.display = 'block';
+	if (screen.width <= 599) {
+		if (menu2.style.display == 'block') {
+			menu.style.display = 'block';
+			menu2.style.display = 'none';
+			boton1.style.display = 'block';
+			boton2.style.display = 'none';
+		} else {
+			menu.style.display = 'none';
+			menu2.style.display = 'block';
+			boton1.style.display = 'none';
+			boton2.style.display = 'block';
+		}
+		// esto es para el correcto fucionamiento en una patantalla mayor o igual a 600px solo cambia el diplay del menu2 //
+	} else if (menu2.style.display == 'block') {
 		menu2.style.display = 'none';
-		boton1.style.display = 'block';
-		boton2.style.display = 'none';
 	} else {
-		menu.style.display = 'none';
 		menu2.style.display = 'block';
-		boton1.style.display = 'none';
-		boton2.style.display = 'block';
 	}
 }
 
 var gatito = 0;
+var Michi = false;
 
 function gato() {
 	if (gatito === 0) {
 		alert('miauuuu');
 		gatito++;
+		$('#michi').removeClass('icon-cancel-1');
+		$('#michi').addClass('icon-check');
+		Michi = true;
 	}
 }
+
+$(function() {
+	$('#Michi').on('click', function Piojoso() {
+		if (Michi) {
+			alert('miau');
+		} else {
+			alert('no encontrado');
+		}
+	});
+});
