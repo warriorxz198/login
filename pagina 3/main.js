0a08a1884a7443e8fec353bb0cdb503ae5183c24
Darkang <warioxz198@gmail.com>
2020-09-06 11:19:58 -0500

    se eliminaron imagenes y se renombaron otras , se agrego contenido a index.html en la carpeta pagina 3 , en main.js de pagina 3 existe una funcion llamada dialogos la cual crea un gruado con un titulo un texto y una imagen le falta la correcion de la visualizacion correcta de la imagen esta funcion permitira a lo largo separarse de main.js y estar en un archivo aparte ya que podra utilizarse en otras paginas al igual que su estilo css

--- pagina 3/main.js
+++ pagina 3/main.js
@@ -1,7 +1,52 @@
-$(function anteriorProgeso() {
+$(document).ready(function anteriorProgeso() {
     $('#michi').removeClass('icon-cancel-1');
     $('#michi').addClass('icon-check');
-    alert("abres la puerta entra piojoso contigo y cierras la puerta")
+    // alert("abres la puerta entra piojoso contigo y cierras la puerta")
     $(".audio")[2].play();
+});
+
+function dialogos(id, texto, titulo, imagen) {
+    $(id).append('<div class = "dialogo">' + '<h1>' + titulo + '</h1>' + '<img src=' + imagen + '/>' + '<p>' + texto + '</p>' + '<div>')
+};
+
+$(function Boton_dia_noche() {
+    $('.cambio').on('click', function () {
+        $(".cambio").toggleClass("activo");
+        $("#sala").addClass("activo");
+        // $(".audio")[3].play();
+        $("#imagen_gato_negro").replaceWith('<img id="imagen_gato_blanco" src="../img/gato_blanco.jpg"> ');
+        $("#imagen_gato_blanco")
+            .prev().text('al despertar no encuentras por ningun lado al piojoso en su lugar encuentras un gato blanco mirandote fijamente con curiosidad');
+        $("#imagen_gato_blanco")
+            .next().text('esto es una puebra prro')
+            .next().text('esto es la puerba dos antes de la pregunta')
+            .next().css("display", "none")
+        $("#imagen_gato_blanco").append(dialogos('#sala', 'esto es una puebra', 'puebra titulo', '../img/gato_blanco.jpg'))
+
+
+
+    });
+    $('.respuestas').on('click', function () {
+        $(".cambio").css("display", "flex");
+    });
+});
+
+
+$(function () {
+    let respuesta1 = false;
+    $('#sala_respuetaGato_1').on('click', function () {
+        if (respuesta1 == false) {
+            $("#sala_respuetaGato_2").css("display", "none")
+            $("#sala").append("<p>-por supuesto que no es mas supongo que mi nuevo dueño estara contento de verte</p>")
+            respuesta1 = true;
+        }
+    });
+    $('#sala_respuetaGato_2').on('click', function () {
+        if (respuesta1 == false) {
+            $("#sala_respuetaGato_1").css("display", "none")
+            $("#sala").append("<p>quedate no quiero que quedarme solito</p>")
+            respuesta1 = true;
+        }
+    });
 })
 
